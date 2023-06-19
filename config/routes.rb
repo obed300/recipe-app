750c5ac6890296d4474a29de0e33e38003291691
josuerushanika <josuerushanika@gmail.com>
2023-06-19 22:12:22 +0200

    Update users devices

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#hello'
   get '/greeting', to: 'custom_pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

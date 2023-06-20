73d8fcc54afa1bcc2069fe584cac43a1768ed01f
obed300 <obamfo@st.ug.edu.gh>
2023-06-20 09:17:46 +0000

    Make changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
   root 'pages#hello'
   get '/greeting', to: 'custom_pages#hello'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: %i[index show] do
+    resources :foods, only: %i[index show new create] do
+      resources :recipes, only: %i[index show new create] do
+        resources :recipes_food, only: %i[index show new create]
+      end
+    end
+  end
 end
--- db/migrate/20230619183629_create_foods.rb
+++ db/migrate/20230619183629_create_foods.rb
@@ -1,11 +1,11 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
-         t.string :name
-         t.string :measurement_unit
-         t.string :price
-         t.integer :quantity
-         t.references :user, index: true, null: false, foreign_key: { to_table: :users }
+      t.string :name
+      t.string :measurement_unit
+      t.string :price
+      t.integer :quantity
+      t.references :user, index: true, null: false, foreign_key: { to_table: :users }
 
       t.timestamps
     end

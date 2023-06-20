3acaa0bcda28e42d3582cb7e12d01be47e53364b
josuerushanika <josuerushanika@gmail.com>
2023-06-20 19:02:07 +0200

    Update recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,38 @@ class RecipesController < ApplicationController
     #GET /recipes or /recipes.json
     def index
         @recipes = current_user.recipes
+    rescue NoMethodError
+        redirect_to new_user_session_path  
     end
 
     #GET /recipes/1 or /recipes/1.json
 
     def show
-        @ingredients = RecipeFood.includes(:food).where(recipe_id: params[:id])
+        @recipes = Recipe.includes(:foods).find(params[:id])
+        @foods = @recipe.foods
     end
 
+    def new
+        @recipe = Recipe.new
+    end
+
+    def create
+        @recipe = current_user.recipes.new(recipe_params)
+
+        if @recipe.save
+            redirect_to recipes_path, notice: 'Recipes created successfully'
+        else
+           render :new
+        end     
+    end
+    
+    def destroy
+        @recipe = current_user.recipe.find(params[:id])
+
+        if @recipe.destroy
+            redirect_to recipes_path, notice: 'Recipe has been removed'
+        else
+            redirect_to recipes_path, notice: 'Recipe could not be deleted'   
+        end 
+    end
 end

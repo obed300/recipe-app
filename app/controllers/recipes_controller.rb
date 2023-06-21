8c5e12c14f99f164b7e018172b5d98a288c9b34d
josuerushanika <josuerushanika@gmail.com>
2023-06-21 17:33:21 +0200

    Update route and recipe controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -35,6 +35,15 @@ class RecipesController < ApplicationController
       redirect_to user_recipes_path, notice: 'Recipe could not be deleted'
     end
   end
+   
+  
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.public = !@recipe.public
+    @recipe.save
+    redirect_to recipe_path(@recipe), notice: 'Recipe status updated'
+  end
+
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   Rails.application.routes.draw do
+    patch 'recipes/:id', to: 'recipes#update', as: 'recipe'
+    
     resources :users, only: %i[index show] do
       get 'shopping_list', to: 'shopping_lists#index', as: 'users_shopping_list'
+      
       resources :foods, only: %i[index show new create]
+      
       resources :recipes, only: %i[index show new create destroy] do
         resources :foods, only: %i[new create]
       end
     end
   end
-  
-end
+end  

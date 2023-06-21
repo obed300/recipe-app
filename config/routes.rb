eb3e774fcbe390fe961c3f8b94e4cddaa405e74c
josuerushanika <josuerushanika@gmail.com>
2023-06-21 17:38:09 +0200

    Update recipe and route

--- config/routes.rb
+++ config/routes.rb
@@ -12,15 +12,17 @@ Rails.application.routes.draw do
   # root "articles#index"
   Rails.application.routes.draw do
     patch 'recipes/:id', to: 'recipes#update', as: 'recipe'
-    
+  
     resources :users, only: %i[index show] do
       get 'shopping_list', to: 'shopping_lists#index', as: 'users_shopping_list'
-      
+  
       resources :foods, only: %i[index show new create]
-      
+  
       resources :recipes, only: %i[index show new create destroy] do
         resources :foods, only: %i[new create]
       end
     end
+    
+    get '/recipes/:id', to: 'recipes#show', as: 'recipe_show'
   end
 end  

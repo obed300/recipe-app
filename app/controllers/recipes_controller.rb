084536574d91ccbca7327c37681df3cb5dc4f593
josuerushanika <josuerushanika@gmail.com>
2023-06-21 10:36:12 +0200

    Fix linters

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,45 +1,44 @@
 class RecipesController < ApplicationController
+  # GET /recipes or /recipes.json
+  def index
+    @recipes = current_user.recipes
+  rescue NoMethodError
+    redirect_to new_user_session_path
+  end
 
-    #GET /recipes or /recipes.json
-    def index
-        @recipes = current_user.recipes
-    rescue NoMethodError
-        redirect_to new_user_session_path  
+  # GET /recipes/1 or /recipes/1.json
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = current_user.recipes.new(recipe_params)
+
+    if @recipe.save
+      redirect_to user_recipes_path, notice: 'Recipes created successfully'
+    else
+      render :new
     end
+  end
 
-    #GET /recipes/1 or /recipes/1.json
+  def destroy
+    @recipe = current_user.recipes.find(params[:id])
 
-    def show
-        @recipe = Recipe.find(params[:id])
+    if @recipe.destroy
+      redirect_to user_recipes_path, notice: 'Recipe has been removed'
+    else
+      redirect_to user_recipes_path, notice: 'Recipe could not be deleted'
     end
+  end
 
-    def new
-        @recipe = Recipe.new
-    end
+  private
 
-    def create
-        @recipe = current_user.recipes.new(recipe_params)
-
-        if @recipe.save
-            redirect_to user_recipes_path, notice: 'Recipes created successfully'
-        else
-           render :new
-        end     
-    end
-    
-    def destroy
-        @recipe = current_user.recipes.find(params[:id])
-
-        if @recipe.destroy
-            redirect_to user_recipes_path, notice: 'Recipe has been removed'
-        else
-            redirect_to user_recipes_path, notice: 'Recipe could not be deleted'   
-        end 
-    end
-
-    private 
-
-    def recipe_params
-        params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
-    end
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
+  end
 end

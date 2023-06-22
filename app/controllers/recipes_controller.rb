1206039c5a8bcdc91ac39bc0bfebf09717f7e39f
obed300 <obamfo@st.ug.edu.gh>
2023-06-22 10:32:01 +0000

    Make changes

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,15 +1,66 @@
+# class RecipesController < ApplicationController
+#   # GET /recipes or /recipes.json
+#   def index
+#     @recipes = current_user.recipes
+#   rescue NoMethodError
+#     redirect_to new_user_session_path
+#   end
+
+#   # GET /recipes/1 or /recipes/1.json
+
+#   def show
+#     @recipe = Recipe.find(params[:id])
+#   end
+
+#   def new
+#     @recipe = Recipe.new
+#   end
+
+#   def create
+#     @recipe = current_user.recipes.new(recipe_params)
+
+#     if @recipe.save
+#       redirect_to user_recipes_path, notice: 'Recipes created successfully'
+#     else
+#       render :new
+#     end
+#   end
+
+#   def destroy
+#     @recipe = current_user.recipes.find(params[:id])
+
+#     if @recipe.destroy
+#       redirect_to user_recipes_path, notice: 'Recipe has been removed'
+#     else
+#       redirect_to user_recipes_path, notice: 'Recipe could not be deleted'
+#     end
+#   end
+
+#   def update
+#     @recipe = Recipe.find(params[:id])
+#     @recipe.public = !@recipe.public
+#     @recipe.save
+#     redirect_to recipe_path(@recipe), notice: 'Recipe status updated'
+#   end
+
+#   private
+
+#   def recipe_params
+#     params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description)
+#   end
+# end
+
 class RecipesController < ApplicationController
   # GET /recipes or /recipes.json
   def index
-    @recipes = current_user.recipes
-  rescue NoMethodError
-    redirect_to new_user_session_path
+    @recipes = Recipe.all
   end
 
   # GET /recipes/1 or /recipes/1.json
 
   def show
     @recipe = Recipe.find(params[:id])
+    @user = User.find(params[:user_id])
   end
 
   def new
@@ -17,12 +68,15 @@ class RecipesController < ApplicationController
   end
 
   def create
-    @recipe = current_user.recipes.new(recipe_params)
+    @user = User.find(params[:user_id])
+    @recipe = @user.recipes.new(recipe_params)
 
-    if @recipe.save
-      redirect_to user_recipes_path, notice: 'Recipes created successfully'
-    else
-      render :new
+    respond_to do |format|
+      if @recipe.save
+        format.html { redirect_to user_food_recipes_path(@user), notice: 'Recipe created successfully' }
+      else
+        format.html { render :new }
+      end
     end
   end
 
@@ -30,17 +84,10 @@ class RecipesController < ApplicationController
     @recipe = current_user.recipes.find(params[:id])
 
     if @recipe.destroy
-      redirect_to user_recipes_path, notice: 'Recipe has been removed'
+      redirect_to user_food_recipes_path, notice: 'Recipe has been removed'
     else
-      redirect_to user_recipes_path, notice: 'Recipe could not be deleted'
+      redirect_to user_food_recipes_path, notice: 'Recipe could not be deleted'
     end
-  end
-
-  def update
-    @recipe = Recipe.find(params[:id])
-    @recipe.public = !@recipe.public
-    @recipe.save
-    redirect_to recipe_path(@recipe), notice: 'Recipe status updated'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,39 +1,24 @@
 Rails.application.routes.draw do
+  # Devise routes for user sessions and registrations
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
 
+  # Root path route
+  root 'welcome#index'
 
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Welcome controller route
+  get 'welcome/index'
 
-  # Defines the root path route ("/")
-  root "foods#index"
-  Rails.application.routes.draw do
-    patch 'recipes/:id', to: 'recipes#update', as: 'recipe'
-
-    resources :users, only: %i[index show] do
-      get 'shopping_list', to: 'shopping_lists#index', as: 'users_shopping_list'
-
-      resources :foods, only: %i[index show new create destroy]
-
-      resources :recipes, only: %i[index show new create destroy] do
-        resources :foods, only: %i[new create]
-      end
-
-  # root 'pages#hello'
-  # get '/greeting', to: 'custom_pages#hello'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root 'foods#index'
+  # User routes
   resources :users, only: %i[index show] do
+    # Food routes
     resources :foods, only: %i[index show new create destroy] do
+      # Recipe routes
       resources :recipes, only: %i[index show new create destroy]
+      # RecipesFoods routes
       resources :recipes_foods, only: %i[index show new create destroy]
-
     end
-
-    get '/recipes/:id', to: 'recipes#show', as: 'recipe_show'
   end
 end
